7192696cbc173b6bdaca1f72a3740373677794a2
na-go <kn.mbk201.mynk@gmail.com>
2023-02-19 17:40:06 +0900

    使ってない変数は_をつけるように

--- variables_3/src/main.rs
+++ variables_3/src/main.rs
@@ -17,43 +17,43 @@ fn variable(gg: i32) {
 
     println!("The value of x is: {}", x);
 
-    let y = 2.0;
+    let _y = 2.0;
 
-    let z: f32 = 3.0;
+    let _z: f32 = 3.0;
 
-    let sum = 5 + 10;
+    let _sum = 5 + 10;
 
-    let difference = 95.5 - 4.3;
+    let _difference = 95.5 - 4.3;
 
-    let product = 4 * 30;
+    let _product = 4 * 30;
 
-    let quotient = 56.7 / 32.2;
-    let floored = 2 / 3;
+    let _quotient = 56.7 / 32.2;
+    let _floored = 2 / 3;
 
-    let remainder = 43 % 5;
-    let t = true;
+    let _remainder = 43 % 5;
+    let _t = true;
 
-    let f: bool = false;
-    let c = 'z';
-    let z = 'ℤ';
-    let heart_eyed_cat = '😻';
+    let _f: bool = false;
+    let _c = 'z';
+    let _z = 'ℤ';
+    let _heart_eyed_cat = '😻';
     let tup: (i32, f64, u8) = (500, 6.4, 1);
 
-    let (x_tup, y_tup, z_tup) = tup;
+    let (_x_tup, y_tup, _z_tup) = tup;
 
     println!("The value of y is: {}", y_tup);
     let x: (i32, f64, u8) = (500, 6.4, 1);
-    let five_hundred = x.0;
+    let _five_hundred = x.0;
 
-    let six_point_four = x.1;
+    let _six_point_four = x.1;
 
-    let one = x.2;
-    let a = [3; 5];
+    let _one = x.2;
+    let _a = [3; 5];
     let a = [1, 2, 3, 4, 5];
-    let months = ["January", "February", "March", "April", "May", "June", "July",
+    let _months = ["January", "February", "March", "April", "May", "June", "July",
               "August", "September", "October", "November", "December"];
-    let first = a[0];
-    let second = a[1];
+    let _first = a[0];
+    let _second = a[1];
 }
 
 fn five() -> i32 {
